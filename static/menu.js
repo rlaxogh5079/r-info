9e78efb27bbc5850a98f3788c744f6522cfd56d3
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-07-11 18:48:29 +0900

    :sparkles: 메뉴 이펙트

--- static/menu.js
+++ static/menu.js
@@ -6,29 +6,35 @@ const info = document.querySelector("#info");
 const skill = document.querySelector("#skill");
 const history = document.querySelector("#history");
 const contact = document.querySelector("#contact");
+const lists = [home, info, skill, history, contact]
 
 bar.addEventListener("mouseenter", (event) => {
     menu.classList.remove("blind");
     menu.classList.add("intro");
     setTimeout(()=> {
-        home.classList.remove("blind");
-        info.classList.remove("blind");
-        skill.classList.remove("blind");
-        history.classList.remove("blind");
-        contact.classList.remove("blind");
+        lists.forEach((list) => {
+            list.classList.remove("blind");
+        })
         menu.classList.remove("intro");
     }, 500)
 })
 
 menu.addEventListener("mouseleave", (event) => {
     menu.classList.add("outro")
-    home.classList.add("blind");
-    info.classList.add("blind");
-    skill.classList.add("blind");
-    history.classList.add("blind");
-    contact.classList.add("blind");
+    lists.forEach((list) => {
+        list.classList.add("blind");
+    })
     setTimeout(()=> {
         menu.classList.remove("outro");
         menu.classList.add("blind");
     }, 400)
+})
+
+lists.forEach((list) => {
+    list.addEventListener("mouseenter", () => {
+        list.classList.add("effect")
+    })
+    list.addEventListener("mouseleave", () => [
+        list.classList.remove("effect")
+    ])
 })
